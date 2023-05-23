49382de9e494faaca01d80f0b1a0cfa5617eecda
giuseppe <giuseppemacc13@gmail.com>
2023-05-23 19:02:04 +0200

    init commit

---
+++ Escape_Room/src/main/java/hasbullateam/escape_room/Escape_Room.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hasbullateam.escape_room;
+
+/**
+ *
+ * @author giuse
+ */
+public class Escape_Room {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
